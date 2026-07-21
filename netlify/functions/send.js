f65741426381df9651b3037159d2659eacac9bb8
RiseryMisha <riserypersonalmail@gmail.com>
2026-07-21 22:42:50 +0300

    Update send.js

--- netlify/functions/send.js
+++ netlify/functions/send.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 chat_id: PARTNER_CHAT_ID,
-                text: "🚨 **Necesito ayuda con esta receta ❤️",
+                text: "🚨 **Necesito ayuda con esta receta 🚨",
                 parse_mode: "Markdown"
             })
         });
